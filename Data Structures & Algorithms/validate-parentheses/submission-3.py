65a78ba057bd7b8c19327bdb4a95cbccba69b266
Tuyishime Hope <85252664+tuyishimehope@users.noreply.github.com>
2026-05-31 00:59:31 +0300

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) <= 1:
+            return False
+
+        start_bracket={"[","{", "(", "<"}
+        end_bracket={"]","}", ")", ">"}
+
+        store=[]
+
+        for char in s:
+            if char in start_bracket:
+                store.append(char)
+
+            if char in end_bracket:
+                if len(store) >= 1:
+                    last = store.pop()
+                else:
+                    return False
+
+                cor=""
+                if last == "[":
+                    cor="]"
+                elif last == "{":
+                    cor="}"
+                elif last == "(":
+                    cor=")"
+
+                if char == cor:
+                    continue
+                return False
+
+        if len(store) >=1:
+            return False
+        return True            
+
+
+        
