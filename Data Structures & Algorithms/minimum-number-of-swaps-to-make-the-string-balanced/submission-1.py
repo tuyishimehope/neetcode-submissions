4fd5a5406910041d193e02e7d4670151f5e13644
Tuyishime Hope <85252664+tuyishimehope@users.noreply.github.com>
2026-09-02 14:05:40 +0300

    Add: minimum-number-of-swaps-to-make-the-string-balanced - submission-1

---
+++ Data Structures & Algorithms/minimum-number-of-swaps-to-make-the-string-balanced/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        unmatched = 0
+
+        for c in s:
+            if c == '[':
+                unmatched += 1
+            elif unmatched:
+                unmatched -= 1
+
+        return (unmatched + 1) // 2
